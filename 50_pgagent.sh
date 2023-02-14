a9c82cf250965ff81546f030634c7ebd43b2a59f
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-02-14 13:26:50 +0100

    Update 50_pgagent.sh

--- 50_pgagent.sh
+++ 50_pgagent.sh
@@ -6,7 +6,7 @@ set -e
 export PGUSER="$POSTGRES_USER"
 
 # Load PGAGENT into $POSTGRES_DB
-echo "Loading POINTCLOUD extensions into $DB"
+echo "Loading PGAGENT extensions into $DB"
 "${psql[@]}" --dbname="$POSTGRES_DB" <<-'EOSQL'
 	CREATE EXTENSION pgagent;
 EOSQL
