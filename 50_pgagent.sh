0dd14afa5f44e701cc767170ecfd6dfb664fff46
Henrik Uggla <henrik.uggla@kristianstad.se>
2023-02-15 10:59:18 +0100

    Create 50_pgagent.sh

---
+++ 50_pgagent.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+# Perform all actions as $POSTGRES_USER
+export PGUSER="$POSTGRES_USER"
+
+# Load PGAGENT into $POSTGRES_DB
+echo "Loading PGAGENT extensions into $DB"
+"${psql[@]}" --dbname="$POSTGRES_DB" <<-'EOSQL'
+	CREATE EXTENSION pgagent;
+EOSQL
